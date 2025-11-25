e4469cd9e787627e6214a6e6b73da11104bed332
Sandman2001 <66444134+Sandman2001@users.noreply.github.com>
2025-11-25 13:34:16 -0600

    commented out ~DB in db.c

--- db.C
+++ db.C
@@ -468,21 +468,21 @@ DB::~DB()
 {
   // this could leave some open files open.
   // need to fix this by iterating through the hash table deleting each open file
-  for (int i = 0; i < HTSIZE; i++) {
-    fileHashBucket* tmpBuc = openFiles.ht[i];
+  // for (int i = 0; i < HTSIZE; i++) {
+  //   fileHashBucket* tmpBuc = openFiles.ht[i];
 
-    while (tmpBuc) {
-      tmpBuc = ht[i];
-      ht[i] = ht[i]->next;
+  //   while (tmpBuc) {
+  //     tmpBuc = ht[i];
+  //     ht[i] = ht[i]->next;
 
-      if (tmpBuc->file != NULL) {
-        tmpBuc->file->close();
-        delete tmpBuc->file;
-      }
+  //     if (tmpBuc->file != NULL) {
+  //       tmpBuc->file->close();
+  //       delete tmpBuc->file;
+  //     }
 
-      delete tmpBuc;
-    }
-  }
+  //     delete tmpBuc;
+  //   }
+  // }
 }
 
 
